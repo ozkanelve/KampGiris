37022477eb6c277863782716e847fea367b85549
ByPatriot <ByPatriot@DESKTOP-K122GBK>
2021-01-16 23:49:11 +0300

    16 Ocak Ders Sonu

---
+++ Donguler/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Donguler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Array - Diziler
+            string[] kurslar = new string[] {"Yazılım Geliştirici Yetiştirme Kamp Kursu",
+                "Programlamaya Başlangıç","Java","Python" };
+            
+            //Döngü
+            for (int i = 0; i <kurslar.Length; i++)
+            {
+                Console.WriteLine(kurslar[i]);
+            }
+            Console.WriteLine("--Sayfa Sonu--");
+
+            //foreach
+            foreach (string kurs in kurslar)
+            {
+                Console.WriteLine(kurs);
+            }
+            Console.WriteLine("--Foreach Bitti--");
+        }
+    }
+}
---
+++ KampGiris/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace KampGiris
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           string kategoriEtiketi= "Kategori";
+            int ogrenciSayisi = 3200;
+            double faizOrani = 1.45;
+            bool sistemeGirisYapmisMi = true;
+            double dolarDun = 7.45;
+            double dolarBugun = 7.45;
+
+            if (dolarDun>dolarBugun)
+            {
+                Console.WriteLine("Azalış Butonu");
+            }
+            else if (dolarDun<dolarBugun)
+            {
+                Console.WriteLine("Artış Butonu");
+            }
+            else
+            {
+                Console.WriteLine("Değişmedi");
+            }
+      
+
+
+            if (sistemeGirisYapmisMi == true)
+            {
+                Console.WriteLine("Kullanıcı Ayarları Butonu");
+            }
+            else
+            {
+                Console.WriteLine("Giriş Yap Butonu");
+            }
+
+
+            Console.WriteLine(kategoriEtiketi);
+        }
+    }
+}
